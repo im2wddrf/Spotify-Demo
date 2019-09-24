3f537fe452ff5393e7cacd05a76a1992f716ad35
Christian Valenzuela <chrisandre95@gmail.com>
2019-09-23 17:43:35 -0700

    changed the scope to allow for newer features

--- auth/authorization_code/app.js
+++ auth/authorization_code/app.js
@@ -47,7 +47,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-read-playback-state user-read-currently-playing playlist-read-private user-library-read';
+  var scope = 'user-read-private user-read-email user-read-playback-state user-read-currently-playing playlist-read-private user-library-read user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
